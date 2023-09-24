1488c88343d51c284927c6ffb5c87794a69ea208
SaLily69 <Hyuri.proS8@gmail.com>
2023-09-24 18:46:57 +0900

    firs-tcommit

---
+++ src/test/java/com/example/todoapisah/TodoApiSahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoapisah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiSahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
